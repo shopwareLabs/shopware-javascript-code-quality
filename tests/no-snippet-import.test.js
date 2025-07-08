79d1fd1f0b1864f551b84651adae5d2ff84341b8
Soner Sayakci <s.sayakci@gmail.com>
2025-07-08 10:09:38 +0200

    update vitest

--- tests/no-snippet-import.test.js
+++ tests/no-snippet-import.test.js
@@ -1,37 +1,49 @@
 import { RuleTester } from "eslint";
+import { describe, it } from "vitest";
 import rule from "../no-snippet-import";
 
-const ruleTester = new RuleTester({
-	languageOptions: { ecmaVersion: 2015, sourceType: "module" },
-});
+describe("no-snippet-import", () => {
+  const ruleTester = new RuleTester({
+    languageOptions: { ecmaVersion: 2015, sourceType: "module" },
+  });
 
-ruleTester.run("no-snippet-import", rule, {
-	valid: [
-		{
-			code: `Shopware.Module.register('my-module', { routes: {} })`,
-		},
-		{
-			code: `Module.register('my-module', { routes: {} })`,
-		},
-	],
-	invalid: [
-		{
-			code: `Shopware.Module.register('my-module', { snippets: {} })`,
-			errors: [
-				{
-					message:
-						"Passing 'snippets' to Shopware.Module.register is forbidden as it increases the bundle size. Snippets are automatically loaded when they are placed in a folder named snippet.",
-				},
-			],
-		},
-		{
-			code: `Module.register('my-module', { snippets: {} })`,
-			errors: [
-				{
-					message:
-						"Passing 'snippets' to Shopware.Module.register is forbidden as it increases the bundle size. Snippets are automatically loaded when they are placed in a folder named snippet.",
-				},
-			],
-		},
-	],
+  it("should be a valid module registration", () => {
+    ruleTester.run("no-snippet-import", rule, {
+      valid: [
+        {
+          code: `Shopware.Module.register('my-module', { routes: {} })`,
+        },
+        {
+          code: `Module.register('my-module', { routes: {} })`,
+        },
+      ],
+      invalid: [],
+    });
+  });
+
+  it("should be an invalid module registration", () => {
+    ruleTester.run("no-snippet-import", rule, {
+      valid: [],
+      invalid: [
+        {
+          code: `Shopware.Module.register('my-module', { snippets: {} })`,
+          errors: [
+            {
+              message:
+                "Passing 'snippets' to Shopware.Module.register is forbidden as it increases the bundle size. Snippets are automatically loaded when they are placed in a folder named snippet.",
+            },
+          ],
+        },
+        {
+          code: `Module.register('my-module', { snippets: {} })`,
+          errors: [
+            {
+              message:
+                "Passing 'snippets' to Shopware.Module.register is forbidden as it increases the bundle size. Snippets are automatically loaded when they are placed in a folder named snippet.",
+            },
+          ],
+        },
+      ],
+    });
+  });
 });
--- tests/no-src-import.test.js
+++ tests/no-src-import.test.js
@@ -1,19 +1,33 @@
 import { RuleTester } from "eslint";
+import { describe, it } from "vitest";
 import rule from "../no-src-import";
-const ruleTester = new RuleTester({
-	languageOptions: { ecmaVersion: 2015, sourceType: "module" },
-});
-ruleTester.run("no-src-import", rule, {
-	valid: [{ code: `import foo from 'src/foo'` }],
-	invalid: [
-		{
-			code: `import foo from '@administration/src/foo'`,
-			errors: [
-				{
-					message:
-						'You can\'t use imports directly from the Shopware Core via "@administration/src/foo". Use the global Shopware object directly instead (https://developer.shopware.com/docs/guides/plugins/plugins/administration/the-shopware-object)',
-				},
-			],
-		},
-	],
+
+describe("no-src-import", () => {
+  const ruleTester = new RuleTester({
+    languageOptions: { ecmaVersion: 2015, sourceType: "module" },
+  });
+
+  it("should be a valid import", () => {
+    ruleTester.run("no-src-import", rule, {
+      valid: [{ code: `import foo from 'src/foo'` }],
+      invalid: [],
+    });
+  });
+
+  it("should be an invalid import", () => {
+    ruleTester.run("no-src-import", rule, {
+      valid: [],
+      invalid: [
+        {
+          code: `import foo from '@administration/src/foo'`,
+          errors: [
+            {
+              message:
+                'You can\'t use imports directly from the Shopware Core via "@administration/src/foo". Use the global Shopware object directly instead (https://developer.shopware.com/docs/guides/plugins/plugins/administration/the-shopware-object)',
+            },
+          ],
+        },
+      ],
+    });
+  });
 });
